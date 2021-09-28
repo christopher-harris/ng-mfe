0ab6ed6bf363ecf3c101afae2e8b1971f7d9dfd1
Christopher Harris <ccharris0607@outlook.com>
2021-09-28 18:20:37 -0500

    Adds Angular Material component library and basic styling. (#1)

--- apps/login/src/app/app.module.ts
+++ apps/login/src/app/app.module.ts
@@ -5,20 +5,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { UiModule } from '@ng-mfe/ui';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CommonModule,
     BrowserModule,
+    UiModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {
         path: '',
         loadChildren: () => import('./remote-entry/entry.module').then((m) => m.RemoteEntryModule)
       }
-    ], { initialNavigation: 'enabledBlocking' }),
+    ], { initialNavigation: 'enabledBlocking' })
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
